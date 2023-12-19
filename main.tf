506d4a47382179ad55f5e4ba7f38cdc5857d3aa5
asungy <62207329+asungy@users.noreply.github.com>
2023-12-19 01:14:20 -0500

    instance with security group

--- main.tf
+++ main.tf
@@ -1,11 +1,35 @@
 provider "aws" {
   region = "us-east-1"
   # Oh no! I spilled my keys!
-  access_key = "AKIA4TJMYTRGR2YHM4KU"
-  secret_key = "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"
+  access_key = "AKIASDUXIESF5SMHJVGZ"
+  secret_key = "B2KAEQ+RbNbZ0H9g1m9PSbJ/pbZS/JwB7hQ0uAAQ"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-079db87dc4c10ac91"
+  ami = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello world" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-sg"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
