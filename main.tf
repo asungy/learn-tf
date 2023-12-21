48d2927af82bc001a9ba1c9a48dddc197264c301
asungy <62207329+asungy@users.noreply.github.com>
2023-12-21 00:55:07 -0500

    Managing state with remote backend (S3)

--- main.tf
+++ main.tf
@@ -1,146 +1,71 @@
+# To destroy state infrastructure, reinitialize terraform backend to use local
+# (use `migrate-state` flag). Set bucket to `force_destroy = true`, run
+# `terraform apply`. And then `terraform destroy` can be run without error.
+
+# terraform {
+#   backend "s3" {
+#     access_key = "AKIAZRPVOCY6J47N5LVE"
+#     secret_key = "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"
+#
+#     bucket = "bucket-56479b3d-64ca-4390-a163-371394418353"
+#     key = "global/s3/terraform.tfstate"
+#     region = "us-east-1"
+#
+#     dynamodb_table = "terraform-locks"
+#     encrypt = true
+#   }
+# }
+#
 provider "aws" {
   region = "us-east-1"
   # Oh no! I spilled my keys!
-  access_key = "AKIAZ5FQAPSOHMGL6NM3"
-  secret_key = "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"
+  access_key = "AKIAZRPVOCY6J47N5LVE"
+  secret_key = "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"
 }
 
-variable "server_port" {
-  description = "The server port"
-  type = number
-  default = 8080
-}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "bucket-56479b3d-64ca-4390-a163-371394418353"
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-sg"
-
-  ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_launch_configuration" "example" {
-  image_id = "ami-0fc5d935ebf8bc3bc"
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello world" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  force_destroy = true
 
   lifecycle {
-    create_before_destroy = true
+    prevent_destroy = false
   }
 }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key = "Name"
-    value = "terraform-asg-example"
-    propagate_at_launch = true
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
-resource "aws_lb" "example" {
-  name = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
-  security_groups = [aws_security_group.alb.id]
-}
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port = 80
-  protocol = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code = 404
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
-resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
-  port = var.server_port
-  protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-  health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-  }
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority = 100
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
 
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
   }
-
-  action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-output "alb_dns_name" {
-  value = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
 }
